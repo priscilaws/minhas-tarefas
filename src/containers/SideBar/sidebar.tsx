2bf1c04fc5e30980d163ffee5782b74a6154c366
Priscila <pri.wagner.silva@gmail.com>
2025-05-23 23:36:43 +0100

    added containers

--- src/containers/SideBar/sidebar.tsx
+++ src/containers/SideBar/sidebar.tsx
@@ -1,3 +1,19 @@
-const SideBar = () => <aside>side bar</aside>
+import CardFilter from '../../components/FilterCards'
+import * as S from './styles'
+const SideBar = () => (
+  <S.Aside>
+    <div>
+      <S.Field type="text" placeholder="Buscar" />
+      <S.Filter>
+        <CardFilter legenda="pendentes" contador={1} />
+        <CardFilter legenda="concluidas" contador={2} />
+        <CardFilter legenda="urgentes" contador={3} />
+        <CardFilter legenda="importantes" contador={4} />
+        <CardFilter legenda="normal" contador={5} />
+        <CardFilter legenda="todas" contador={10} ativo />
+      </S.Filter>
+    </div>
+  </S.Aside>
+)
 
 export default SideBar
