52d95292cb7f4815b58f24912ff76c80f271617d
Priscila <pri.wagner.silva@gmail.com>
2025-05-23 23:35:57 +0100

    added task list

---
+++ src/containers/TaskList/styles.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const CardContainer = styled.main`
+  padding: 0px 40px;
+  height: 100vh;
+  overflow-y: scroll;
+`
--- src/containers/TaskList/task-list.tsx
+++ src/containers/TaskList/task-list.tsx
@@ -1,10 +1,47 @@
+import Task from '../../components/Task'
+import { CardContainer } from './styles'
+import * as enums from '../../utils/enums/Tarefas'
+
+const tarefas = [
+  {
+    titulo: 'Estudar aula Typescript',
+    descricao: 'Ver aula 3 EBAC',
+    prioridade: enums.Prioridade.IMPORTANTE,
+    status: enums.Status.PENDENTE
+  },
+  {
+    titulo: 'Pagar conta internet',
+    descricao: 'Baixar fatura',
+    prioridade: enums.Prioridade.URGENTE,
+    status: enums.Status.CONCLUIDA
+  },
+  {
+    titulo: 'academia',
+    descricao: 'treino B',
+    prioridade: enums.Prioridade.IMPORTANTE,
+    status: enums.Status.PENDENTE
+  }
+]
+
 const TaskList = () => (
-  <main>
+  <CardContainer>
     <p>
       {' '}
       2 Tarefas marcadas como: &quot;categoria&ldquo; e &quot;termo&ldquo;{' '}
     </p>
-  </main>
+    <ul>
+      {tarefas.map((t) => (
+        <li key={t.titulo}>
+          <Task
+            descricao={t.descricao}
+            titulo={t.titulo}
+            status={t.status}
+            prioridade={t.prioridade}
+          />
+        </li>
+      ))}
+    </ul>
+  </CardContainer>
 )
 
 export default TaskList
